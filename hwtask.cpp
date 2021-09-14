eb5dfd47e89e81f02082ccf879cd69031dcfe527
arsen-arsen <72999223+arsen-arsen@users.noreply.github.com>
2021-09-14 15:15:10 +0300

    Create hwtask.cpp

---
+++ hwtask.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cstring>
+#include <iostream>
+#include <Windows.h> 
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+
+//Вариант 1
+
+int const MaxSize = 10;
+class FooString
+{
+private:
+    char* buf;
+public:
+    FooString();
+    ~FooString();
+    void show();
+    int length();
+    int testInput();
+    void testData();
+};
+ 
+FooString::FooString()
+{
+    buf = new char[MaxSize];
+    testData();
+}
+
+FooString::~FooString()
+{
+    delete[]buf;
+}
+
+void FooString::testData()
+{
+    cout << "Enter the string: ";
+    cin.getline(buf, MaxSize);
+}
+
+int FooString::length()
+{
+    int i=0;
+    while (true)
+    {
+        if (buf[i] == '\0')
+            break;
+        else
+            i++;
+    }
+    i++;
+    return i;
+}
+
+void FooString::show()
+{
+    int l = length();
+    cout << "String length: " << l << endl;
+    for (int i = 0; i < l; i++)
+        cout << buf[i];
+}
+
+
+
+int FooString::testInput() 
+{
+    if (cin.fail()) 
+    {
+        cin.clear(); 
+        cin.ignore(32767, '\n'); 
+        cout << "Error" << endl;
+        return -1;
+    }
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    FooString mas;
+    if (mas.testInput() == -1)
+            return -1;
+    mas.show();
+}
